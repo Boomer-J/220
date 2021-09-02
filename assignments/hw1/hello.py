a8b3e1ecbde6b2733a66455159fdb85efccd9a5b
Boomer-J <jenksb@g.cofc.edu>
2021-09-02 15:24:07 -0400

    Completed hw1

---
+++ assignments/hw1/hello.py
@@ -0,0 +1,11 @@
+"""
+name: Jenks, Boomer
+hello.py
+
+Problem: write a program with a main function that prints “hello, world!” to the screen
+
+Certification of authenticity:
+I certify that this assignment is entirely my own work.
+"""
+def main():
+    print("hello, world!")
